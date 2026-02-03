c4404ddda56d95ed9973298867ad1f07d69d7386
planetofangels777 <planetofangels777@gmail.com>
2026-02-03 17:07:02 +0100

    Add files via upload

---
+++ src/main/java/name/modid/Presencefootsteps.java
@@ -0,0 +1,10 @@
+package name.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Presencefootsteps implements ModInitializer {
+	public static final String MOD_ID = "presencefootsteps";
+	@Override
+	public void onInitialize() {
+	}
+}
